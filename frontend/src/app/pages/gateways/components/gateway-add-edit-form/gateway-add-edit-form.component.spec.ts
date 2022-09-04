41f10b2581aee8e57f2ef5a48097b1203545bc3a
ernesthm <ernestoh2006@gmail.com>
2022-09-04 16:50:35 -0400

    starting unit testings

--- frontend/src/app/pages/gateways/components/gateway-add-edit-form/gateway-add-edit-form.component.spec.ts
+++ frontend/src/app/pages/gateways/components/gateway-add-edit-form/gateway-add-edit-form.component.spec.ts
@@ -7,7 +7,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GatewayAddEditFormComponent } from './gateway-add-edit-form.component';
 import { ToastrService } from 'ngx-toastr';
-import { toastrService } from '@/utils/testing';
+import { toastrService, fakeTyping } from '@/utils/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 describe('GatewayAddEditFormComponent', () => {
@@ -29,10 +29,56 @@ describe('GatewayAddEditFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GatewayAddEditFormComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should ipv4 field be valid when entering a good ip', () => {
+    let input = fixture.debugElement.query(By.css('#ipv4'));
+    input.nativeElement.focus();
+    fakeTyping('10.0.0.1', input.nativeElement);
+
+    fixture.detectChanges();
+
+    expect(!component.ipv4.invalid).toBeTruthy();
+  });
+
+  it('should ipv4 field be invalid when entering bad ip', () => {
+    let input = fixture.debugElement.query(By.css('#ipv4'));
+    input.nativeElement.focus();
+    fakeTyping('10.0.0.3333', input.nativeElement);
+
+    fixture.detectChanges();
+
+    expect(component.ipv4.invalid).toBeTruthy();
+  });
+
+  it('should form be invalid when bad input', () => {
+    let input = fixture.debugElement.query(By.css('#ipv4'));
+    fakeTyping('10.0.0.3333', input.nativeElement);
+
+    fixture.detectChanges();
+
+    expect(component.gatewayForm.invalid).toBeTruthy();
+  });
+
+  it('should form be valid on input ok', () => {
+    let ipv4 = fixture.debugElement.query(By.css('#ipv4'));
+    let name = fixture.debugElement.query(By.css('#name'));
+    let sn = fixture.debugElement.query(By.css('#serialNumber'));
+
+    fakeTyping('10.0.0.1', ipv4.nativeElement);
+    fakeTyping('Gateway 1', name.nativeElement);
+    fakeTyping('00 55 66 77', sn.nativeElement);
+
+
+    fixture.detectChanges();
+
+    expect(component.gatewayForm.valid).toBeTruthy();
   });
 });
--- frontend/src/app/utils/testing.ts
+++ frontend/src/app/utils/testing.ts
@@ -12,3 +12,23 @@ export const toastrService = {
     override?: Partial<IndividualConfig>
   ) => {},
 };
+
+export function fakeTyping(value: string, inputEl: HTMLInputElement) {
+  let result: string = '';
+  for (let char of value) {
+    let eventMock = createKeyDownEvent(char);
+    inputEl.dispatchEvent(eventMock);
+    if (eventMock.defaultPrevented) {
+      // invalid char
+    } else {
+      result = result.concat(char);
+    }
+  }
+
+  inputEl.value = result;
+  inputEl.dispatchEvent(new Event('input'));
+}
+
+export function createKeyDownEvent(value: string, cancelable = true) {
+  return new KeyboardEvent('keydown', { key: value, cancelable });
+}
